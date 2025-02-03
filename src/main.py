013533db4a398b5fa0daf8668e4bcced7fc4b452
Yash <yash.ranjan25@gmail.com>
2025-02-03 15:41:15 +0530

    initial setup of the project

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode
+
+def main():
+    new_node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(new_node)
+    
+if __name__ == "__main__":
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL = "normal"
+    BOLD = "bold"
+    ITALICS = "italics"
+    CODE = "code"
+    LINK = "link"
+    IMAGES = "images"
+
+class TextNode():
+    """
+        Used for setting up an HTML text node.
+
+        Args:
+            text: `string` => The string content of the TextNode
+            text_type: `TextType` => The type of content the node represent, should use a type available in the `TextType` enum
+            url (optional): `string` => URL for cases where the element needs a url (links or images)
+    """
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+    
+    def __eq__(self, other_node):
+        if (other_node.text == self.text and other_node.text_type == self.text_type and other_node.url == self.url):
+           return True
+        return False 
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
