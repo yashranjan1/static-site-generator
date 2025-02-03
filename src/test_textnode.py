ecbed52e8f0d4bfd0b9201d786d23fc32e0e8856
Yash <yash.ranjan25@gmail.com>
2025-02-03 15:57:38 +0530

    added unittests

---
+++ src/test_textnode.py
@@ -0,0 +1,33 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        # when both are equal
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+        
+        # when the content is not the name
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a slightly different text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+        
+        # when the type is not the same
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.IMAGES)
+        self.assertNotEqual(node, node2)
+        
+        # when both have links but the link is not the same 
+        node = TextNode("This is a text node", TextType.LINK, url="https://example.com")
+        node2 = TextNode("This is a text node", TextType.LINK, url="https://notexample.com")
+        self.assertNotEqual(node, node2)
+    
+        # when the link for one is defined but isnt for the other 
+        node = TextNode("This is a text node", TextType.LINK, url="https://example.com")
+        node2 = TextNode("This is a text node", TextType.LINK)
+        self.assertNotEqual(node, node2)
+        
+if __name__ == "__main__":
+    unittest.main()
